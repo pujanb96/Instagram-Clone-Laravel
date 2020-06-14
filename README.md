3d701e33f81810e62ee7f178bb16a55df6bab165
Pujan Bhuva <37817715+pujanb96@users.noreply.github.com>
2020-06-14 19:07:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Instagram Clone Laravel (Binstagram)
+ This a laravel web-app that copies the functionality of Instagram
---
+++ app/Profile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage(){
+        $imagePath = ($this->image) ?  $this->image : 'profile/oHrFOPadAUPSwZg8AAEjoWm9cI1w5xSc50OIKJ5j.jpeg';
+        return '/storage/' . $imagePath;
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+
+}
