9fbd5a2f31bc647d6dad4f2232c0f83368a69df1
Pujan Bhuva <37817715+pujanb96@users.noreply.github.com>
2020-06-14 23:49:22 -0300

    Added emailing and fixed bugs

---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to Binstagram
+
+This is a welcome mail from Heisenbug - the creator of Binstagram. You can change this code in welcome-email.blade.php file to write your own welcome email for users.
+
+
+
+All the best,<br>
+Heisenbug (Binstagram)
+@endcomponent
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,7 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-4">
                 <div class="h4">{{ $user->username  }}</div>
-                    <follow-button></follow-button>
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
                 </div>
 
                 @can('update', $user->profile)
@@ -23,9 +23,9 @@
             @endcan
 
             <div class="d-flex">
-                <div class="pr-5"><strong>{{ $user->posts->count()  }}</strong> posts</div>
-                <div class="pr-5"><strong>23k</strong> followers</div>
-                <div class="pr-5"><strong>23</strong> following</div>
+                <div class="pr-5"><strong>{{ $postCount }}</strong> posts</div>
+                <div class="pr-5"><strong>{{ $followerCount }}</strong> followers</div>
+                <div class="pr-5"><strong>{{ $followingCount  }}</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title  }}</div>
             <div>{{ $user->profile->description  }}</div>
